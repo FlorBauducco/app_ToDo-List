3900dc1422ada5055ec35fc4121553ef7c72e1fc
Flor <florazul.fb@gmail.com>
2025-03-27 23:24:18 +0100

    Proyecto To-Do list con JS,HMTL,CSS v1

--- js/main.js
+++ js/main.js
@@ -1,33 +1,32 @@
-/* var nombrePerrito = "Simon"; // Variable non-block scope (puede ser accedidos en cualquer parte del codigo) - reutilizar y renombrar
-let nombrePersona = "Flor"; // Variables block scope (pueden ser modificadas dentro de un bucle, fuera no) - reutilizar y renombrar
-const descuentos = 20; // Variables constantes - block scoped - reutilizar pero no renombrar */
+const button = document.querySelector(".task-submit");
+const taskInput = document.querySelector(".task-input");
+const taskList = document.querySelector(".task-list");
 
-// Novio te ama
-// Modulo operator %
-//Numeros del 1 al 20 - si es par o impar
+button.addEventListener("click", function () {
+  const taskText = taskInput.value.trim();
 
-// for (let i = 0; i <= 20; i++) {
-//   if (i % 2 === 0) {
-//     console.log(`El número ${i} es par`);
-//   } else {
-//     console.log(`El número ${i} es impar`);
-//   }
-// }
+  if (taskText !== "") {
+    //Si no hay texto en el input, no se crea la tarea
+    // Crear una nueva tarea en la lista, cuando el usuario haga clic en el botón
+    const newTask = document.createElement("li"); // Crea un LI nuevo
+    newTask.textContent = taskText; // Agregar el texto del input al LI
 
-//Ejercicio FizzBuzz - Iteracion del 1 al 100, si el numero es multiplo de 3 'Fizz' , si el numero es multiplo 5 'Buzz?, si el numero es multiplo de los 2 'FizzBuzz' Un ej: 15, 30
+    // Crear un botón para eliminar la tarea
+    const deleteButton = document.createElement("button");
+    deleteButton.textContent = "Eliminar";
+    deleteButton.className = "delete-button";
 
-// for (let i = 0; i <= 100; i++) {
-//   if (i % 3 === 0 && i % 5 === 0) {
-//     console.log(` - ${i} - FizzBuzz`);
-//   } else if (i % 5 === 0) {
-//     console.log(` - ${i} - Buzz`);
-//   } else if (i % 3 === 0) {
-//     console.log(` - ${i} - Fizz`);
-//   }
-// }
+    deleteButton.addEventListener("click", function () {
+      taskList.removeChild(newTask);
+    });
 
-const productos = ["nails", "eyebrows"];
+    // Agregar el boton a la tarea nueva
+    newTask.appendChild(deleteButton);
 
-productos.push("hair");
+    //Agregar la tarea a la lista
+    taskList.appendChild(newTask);
 
-console.log(productos);
+    //Limpiar el input
+    taskInput.value = "";
+  }
+});
